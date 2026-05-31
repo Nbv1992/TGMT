e509c2c4a3946796d19d8970d2d03d9465238a70
Nbv1992 <131999148+Nbv1992@users.noreply.github.com>
2026-05-31 21:26:57 +0700

    Add files via upload

---
+++ lab12.1.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("image.jpg")
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+_, thresh = cv2.threshold(
+    gray,
+    127,
+    255,
+    cv2.THRESH_BINARY
+)
+
+contours, hierarchy = cv2.findContours(
+    thresh,
+    cv2.RETR_TREE,
+    cv2.CHAIN_APPROX_SIMPLE
+)
+
+cv2.drawContours(
+    img,
+    contours,
+    -1,
+    (0, 255, 0),
+    2
+)
+
+cv2.imshow("Contours", img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
